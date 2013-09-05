29555a8f785650d8b672c72c83d4cda9de07517f
Jenny <jenny@simplemama.com>
2013-09-05 12:11:34 -0400

    Initial release

---
+++ follow-me.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	Plugin Name: Follow Me
+	Author: Simple Mama
+	Author URI: http://www.simplemama.com
+	Description: Get more social media followers by adding this section to your website. For PageLines DMS only.
+	Class Name: FollowMe
+	Demo: http://www.simplemama.com
+	Version: 1.0
+	PageLines: true
+	v3: true
+	Filter: social
+*/
